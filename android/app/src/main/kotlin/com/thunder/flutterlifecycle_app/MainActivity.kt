c285b2e0fe7ea3b7096d92629e29f5a5c3458392
Shailebhyanand <shailebhya@gmail.com>
2020-08-14 11:09:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/thunder/flutterlifecycle_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thunder.flutterlifecycle_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+AppLifecycleState _appLifecycleState;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      _appLifecycleState = state;
+      print("My App State is $_appLifecycleState");
+
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: OrientationBuilder(
+          builder: (context,orientation){
+            return  Center(
+            child: Text(
+                _appLifecycleState.toString(),
+                style: TextStyle(
+                  fontSize: 22,
+                  color: orientation== Orientation.portrait ? Colors.blue:Colors.red
+                ),),
+            );
+          }
+
+        ),
+      ),
+    );
+  }
+}
